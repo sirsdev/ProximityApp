1f94c10b54ab2b83d81a730ae46d437d76334c24
sirsdev <sirsdev@gmail.com>
2014-03-18 11:45:54 +0200

    fixx service
    asd

--- src/com/sirsdev/proximity/MainActivity.java
+++ src/com/sirsdev/proximity/MainActivity.java
@@ -2,75 +2,39 @@ package com.sirsdev.proximity;
 
 import android.app.Activity;
 import android.content.Context;
-import android.hardware.Sensor;
-import android.hardware.SensorEvent;
-import android.hardware.SensorEventListener;
-import android.hardware.SensorManager;
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.PowerManager;
-import android.os.SystemClock;
-import android.view.WindowManager;
-import android.widget.Toast;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.ToggleButton;
 
-public class MainActivity extends Activity implements SensorEventListener {
-	private SensorManager mSensorManager;
-	private Sensor mSensor;
-	PowerManager pm;
+public class MainActivity extends Activity {
+	ToggleButton toggle;
+	Intent intent;
+	Context context;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_activity_layout);
-		mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
-		mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
-		pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+		toggle = (ToggleButton) findViewById(R.id.toggleButton1);
+		context = getBaseContext();
+		intent = new Intent(MainActivity.this,ProximityService.class);
+		toggle.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+				@Override
+			public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+				if(isChecked){
+					MainActivity.this.startService(intent);
+				}
+				else
+				{
+					MainActivity.this.stopService(intent);
+				}
+			}
+		});
 	}
 	
-	@Override
-	public void onAccuracyChanged(Sensor sensor, int accuracy) {
-		// TODO Auto-generated method stub
-		
-	}
 	 protected void onResume() {
 		  super.onResume();
-		  try{
-		  mSensorManager.registerListener(this, mSensor,
-		    SensorManager.SENSOR_DELAY_NORMAL);
-		  }
-		  catch(Exception e){
-				Toast toast = Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT);
-				toast.show();
-			}
 	 }
-	@Override
-	public void onSensorChanged(SensorEvent event) {
-		if(event.values[0]==0){
-			//light up for 10 seconds.
-			//testToast
-			try{
-			Toast toast = Toast.makeText(getApplicationContext(), "Near", Toast.LENGTH_SHORT);
-			toast.show();
-			//pm.wakeUp(SystemClock.uptimeMillis());
-			//getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-			}
-			catch(Exception e){
-				Toast toast = Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT);
-				toast.show();
-			}
-			
-		}
-		else{
-			//light up for 10 seconds.
-			try{
-			Toast toast = Toast.makeText(getApplicationContext(), "Far", Toast.LENGTH_SHORT);
-			toast.show();
-			//pm.wakeUp(SystemClock.uptimeMillis());
-			getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
-			}
-			catch(Exception e){
-				Toast toast = Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT);
-				toast.show();
-			}
-		}
-	}
 
 }
--- src/com/sirsdev/proximity/ProximityService.java
+++ src/com/sirsdev/proximity/ProximityService.java
@@ -1,20 +1,91 @@
 package com.sirsdev.proximity;
 
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.IBinder;
+import android.os.PowerManager;
+import android.widget.Toast;
 
-public class ProximityService extends Service{
-
+public class ProximityService extends Service implements SensorEventListener{
+	private SensorManager mSensorManager;
+	private Sensor mSensor;
+	PowerManager pm;
+	@Override
+	public void onCreate() {		
+	mSensorManager = (SensorManager) getSystemService(SENSOR_SERVICE);
+	mSensor = mSensorManager.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+	pm = (PowerManager) getSystemService(Context.POWER_SERVICE);
+	Toast toast = Toast.makeText(getBaseContext(), "Servicestarted", Toast.LENGTH_SHORT);
+	toast.show();
+	  try{
+	  mSensorManager.registerListener(this, mSensor,
+	  SensorManager.SENSOR_DELAY_NORMAL);
+	  }
+	  catch(Exception e){
+			Toast toast2 = Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT);
+			toast2.show();
+		}
+		super.onCreate();
+	}
+	@Override
+	public void onDestroy() {		
+		Toast toast = Toast.makeText(getBaseContext(), "Service Stopperd", Toast.LENGTH_SHORT);
+		toast.show();
+		super.onDestroy();
+	}
 	@Override
 	public IBinder onBind(Intent intent) {
+
 		return null;
 	}
 	
 	  @Override
 	  public int onStartCommand(Intent intent, int flags, int startId) {
-	    //TODO do something useful
-	    return Service.START_NOT_STICKY;
+		    return Service.START_NOT_STICKY;
 	  }
 
+	@Override
+	public void onAccuracyChanged(Sensor sensor, int accuracy) {
+		// TODO Auto-generated method stub
+		
+	}
+	@Override
+	public void onSensorChanged(SensorEvent event) {
+		if(event.values[0]==0){
+			//light up for 10 seconds.
+			//testToast
+			try{
+			Toast toast = Toast.makeText(getApplicationContext(), "Near", Toast.LENGTH_SHORT);
+			toast.show();
+			//pm.wakeUp(SystemClock.uptimeMillis());
+			//getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+			}
+			catch(Exception e){
+				Toast toast = Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT);
+				toast.show();
+			}
+			
+		}
+		else{
+			//light up for 10 seconds.
+			try{
+			Toast toast = Toast.makeText(getApplicationContext(), "Far", Toast.LENGTH_SHORT);
+			toast.show();
+			//pm.wakeUp(SystemClock.uptimeMillis());
+			//getWindow().addFlags(WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+			}
+			catch(Exception e){
+				Toast toast = Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT);
+				toast.show();
+			}
+		}
+	}
+
 }
+
+
